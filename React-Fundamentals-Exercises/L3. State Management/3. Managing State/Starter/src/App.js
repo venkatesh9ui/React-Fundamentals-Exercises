5a1460728a827f663750ae4e6c5d8647cb98c551
venkatesh9ui <venkatesh9ui@gmail.com>
2022-11-13 20:44:15 +0000

    using useState to display the number of likes in the UI

--- React-Fundamentals-Exercises/L3. State Management/3. Managing State/Starter/src/App.js
+++ React-Fundamentals-Exercises/L3. State Management/3. Managing State/Starter/src/App.js
@@ -1,12 +1,15 @@
 import corgi from "./corgi.jpg";
+import {useState} from "react"
 import "./App.css";
 
 const App = () => {
+  const[amountOfLikes, setAmountOfLikes] = useState(0)
   return (
     <div className="container">
       <h2>Like this photo!</h2>
       <img src={corgi} alt="Corgi"></img>
-      <p>Amount of likes: </p>
+      <button onClick={() => setAmountOfLikes(amountOfLikes+1)}>Like</button>
+      <p>Amount of likes: {amountOfLikes}</p>
     </div>
   );
 };
